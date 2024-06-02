7ffef22ab409df946dae3d3f191d74c14a38c1d6
Fernando Navajas <fernandonm07@hotmail.com>
2024-06-02 15:36:37 -0300

    Criado o monitoramento de qualidade da água

--- monitoramento_ambiental.py
+++ monitoramento_ambiental.py
@@ -1,1 +1,21 @@
 # Monitoramento Ambiental
+
+monitoramento_agua = [] # Lista para armazenar dados de qualidade da água
+
+# Função para registrar dados de qualidade da água
+def registrar_monitoramento(data, local, pH, turbidez, hidrocarbonetos):
+    registro = {
+        "data": data,
+        "local": local,
+        "pH": pH,
+        "turbidez": turbidez,
+        "hidrocarbonetos": hidrocarbonetos
+    }
+    monitoramento_agua.append(registro)
+
+# Função para exibir relatórios de monitoramento
+def exibir_relatorio_monitoramento():
+    print("Relatório de Monitoramento de Qualidade da Água")
+    for registro in monitoramento_agua:
+        print(f'Data: {registro['data']}, Local: {registro['local']}, pH: {registro['pH']}, Turbidez: {registro['turbidez']}, Hidrocarbonetos: {registro['hidrocarbonetos']}')
+
