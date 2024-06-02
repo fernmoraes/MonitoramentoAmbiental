b1af697461656b99c544f219ebed42a65edad18c
Fernando Navajas <fernandonm07@hotmail.com>
2024-06-02 15:54:01 -0300

    Criação de testes para resíduos

--- monitoramento_ambiental.py
+++ monitoramento_ambiental.py
@@ -15,6 +15,7 @@ def registrar_monitoramento(data, local, pH, turbidez, hidrocarbonetos):
 
 # Função para exibir relatórios de monitoramento
 def exibir_relatorio_monitoramento():
+    print("")
     print("Relatório de Monitoramento de Qualidade da Água")
     for registro in monitoramento_agua:
         print(f'Data: {registro['data']}, Local: {registro['local']}, pH: {registro['pH']}, Turbidez: {registro['turbidez']}, Hidrocarbonetos: {registro['hidrocarbonetos']}')
@@ -35,20 +36,35 @@ def registrar_residuo(data, tipo, quantidade):
 
 # Função para exibir relatórios de gestão de resíduos
 def exibir_relatorio_residuos():
+    print("")
     print("Relatório de Gestão de Resíduos")
     for registro in gestao_residuos:
         print(f"Data: {registro['data']}, Tipo: {registro['tipo']}, Quantidade: {registro['quantidade']} kg")
+        print("")
 
 # Exemplo de uso do projeto
 
 # Registrando dados de monitoramento ambiental
-data = input("Insira a data: ")
-local = input("Insira o local: ")
+print("Monitoramento da Qualidade da Água")
+print("")
+data_Agua = input("Insira a data: ")
+local_Agua = input("Insira o local: ")
 pH = float(input("Insira o pH: "))
 turbidez = input("Insira a turbidez: ")
 hidrocarbonetos = float(input("Insira os hidrocarbonetos: "))
-registrar_monitoramento(data, local, pH, turbidez, hidrocarbonetos)
+registrar_monitoramento(data_Agua, local_Agua, pH, turbidez, hidrocarbonetos)
 
 # Exibindo relatório de monitoramento ambiental
 
 exibir_relatorio_monitoramento()
+
+# Registrando dados de gestão de resíduos
+print("Monitoramento dos Resíduos")
+print("")
+data_Residuos = input("Insira a data: ")
+tipo_Residuo = input("Insira o tipo do resíduo na água: ")
+quantidade_Residuo = float(input("Insira a quantidade aproximada de resíduos (em Kg): "))
+registrar_residuo(data_Residuos, tipo_Residuo, quantidade_Residuo)
+
+# Exibindo relatório de gestão de resíduos
+exibir_relatorio_residuos()
