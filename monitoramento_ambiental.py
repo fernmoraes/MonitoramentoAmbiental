9c11ace896e0183274db2b9b30c0ab01932e4a16
Fernando Navajas <fernandonm07@hotmail.com>
2024-06-02 15:38:49 -0300

    Criado função de teste do projeto

--- monitoramento_ambiental.py
+++ monitoramento_ambiental.py
@@ -18,4 +18,20 @@ def exibir_relatorio_monitoramento():
     print("Relatório de Monitoramento de Qualidade da Água")
     for registro in monitoramento_agua:
         print(f'Data: {registro['data']}, Local: {registro['local']}, pH: {registro['pH']}, Turbidez: {registro['turbidez']}, Hidrocarbonetos: {registro['hidrocarbonetos']}')
+        print("")
 
+# Gestão de Resíduos
+
+# Exemplo de uso do projeto
+
+# Registrando dados de monitoramento ambiental
+data = input("Insira a data: ")
+local = input("Insira o local: ")
+pH = float(input("Insira o pH: "))
+turbidez = input("Insira a turbidez: ")
+hidrocarbonetos = float(input("Insira os hidrocarbonetos: "))
+registrar_monitoramento(data, local, pH, turbidez, hidrocarbonetos)
+
+# Exibindo relatório de monitoramento ambiental
+
+exibir_relatorio_monitoramento()
